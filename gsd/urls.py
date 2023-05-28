04ea1624f471f32baec7e704d6a2d7228e69a50b
Eric Frey <ericlfrey@gmail.com>
2023-05-28 10:44:02 -0500

    Changed the auth to accept the incoming object from the client side after the FirebaseAuth is verified.

--- gsd/urls.py
+++ gsd/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import include
-from rest_framework import routers
 from django.urls import path
-from gsdapi.views import register_user, login_user, ProjectView, TaskView, MaterialView
+from rest_framework import routers
+from gsdapi.views import register_user, check_user, ProjectView, TaskView, MaterialView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'projects', ProjectView, 'project')
@@ -27,7 +27,7 @@ router.register(r'materials', MaterialView, 'material')
 
 urlpatterns = [
     path('register', register_user),
-    path('login', login_user),
+    path('checkuser', check_user),
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
 ]
