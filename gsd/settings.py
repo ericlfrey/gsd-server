157cba2ddcf9838b9650ebff1f089f19b9565ee5
Eric Frey <ericlfrey@gmail.com>
2023-08-24 15:02:15 -0500

    Updated cors list.

--- gsd/settings.py
+++ gsd/settings.py
@@ -63,7 +63,8 @@ REST_FRAMEWORK = {
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://gsd-client-9c280938cdcf.herokuapp.com/'
 )
 
 MIDDLEWARE = [
